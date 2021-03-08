321fd6a2d2102a4fe04b45647804fa6f8c79e590
Zenzi Ali <zeeathome935@gmail.com>
2021-03-07 21:10:21 -0500

    created 1st filter for GitHub Jobs API

--- JSWSCourse/project-1/index.js
+++ JSWSCourse/project-1/index.js
@@ -1,27 +1,65 @@
 const Sheet = require('./sheet');
 const fetch = require('node-fetch');
 
-(async function() {
-  
+async function scrapePage(i) {
   const res = await fetch(
-    "https://jobs.github.com/positions.json?description=fullstack&location=new+york"
+    `https://jobs.github.com/positions.json?page=${i}&search=code`
   );
   const json = await res.json();
-  
-  const rows = json.map(job => {
+
+  const rows = json.map((job) => {
     return {
       Company: job.company,
       Title: job.title,
       Location: job.location,
       Date: job.created_at,
       URL: job.url,
-    }
-  })
+    };
+  });
+  return rows;
+}
+
+(async function() {
+  let i = 1;
+  let rows = [];
+  while(true) {
+    const newRows = await scrapePage(i);
+    console.log("New Row Length", newRows.length);
+    if (newRows.length === 0) break;
+    rows = rows.concat(newRows);
+    i++;
+  }
+//add sorting function
+//additional filter by keyword (string method includes)
+  console.log("Total Row Length", rows.length);
 
   const sheet = new Sheet();
   await sheet.load();
-
   await sheet.addRows(rows);
-
-  //console.log({json})
 })()
+
+
+
+
+// (async function() {
+//   const res = await fetch(
+//     "https://jobs.github.com/positions.json?page=20&search=code"
+//   );
+//   const json = await res.json();
+  
+//   const rows = json.map(job => {
+//     return {
+//       Company: job.company,
+//       Title: job.title,
+//       Location: job.location,
+//       Date: job.created_at,
+//       URL: job.url,
+//     }
+//   })
+//   // const sheet = new Sheet();
+//   // await sheet.load();
+
+//   // await sheet.addRows(rows);
+
+//   // console.log(rows.length, 'length')
+// })()
---
+++ JSWSCourse/project-1/scrapeBySearch.js
@@ -0,0 +1,25 @@
+const Sheet = require("./sheet");
+const fetch = require("node-fetch");
+
+(async function () {
+  const res = await fetch(
+    "https://jobs.github.com/positions.json?description=node&location=remote"
+  );
+  const json = await res.json();
+
+  const rows = json.map((job) => {
+    return {
+      Company: job.company,
+      Title: job.title,
+      Location: job.location,
+      Date: job.created_at,
+      URL: job.url,
+    };
+  });
+  const sheet = new Sheet();
+  await sheet.load();
+
+  await sheet.addRows(rows);
+
+  console.log(rows.length, "length");
+})();
